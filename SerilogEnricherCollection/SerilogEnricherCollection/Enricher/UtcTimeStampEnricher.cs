d2848aaa548304e28e0f78ffc47e2ac1c077bc9d
ltd65 <lars.dausch@gmail.com>
2018-01-28 08:45:46 +0100

    compilable setup. two target frameworks set. First enricher code in place nuget package is created.

---
+++ SerilogEnricherCollection/SerilogEnricherCollection/Enricher/UtcTimeStampEnricher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace SerilogEnricherCollection.Enricher
+{
+    public class UtcTimestampEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UtcTimestamp", logEvent.Timestamp.UtcDateTime));
+        }
+    }
+}
---
+++ SerilogEnricherCollection/SerilogEnricherCollection/Enricher/UtcTimeStampEnricherConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+
+namespace SerilogEnricherCollection.Enricher
+{
+    public static class UtcTimestampConfigurationExtension
+    {
+        public static LoggerConfiguration WithUtcTimestamp(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            return enrichmentConfiguration.With<UtcTimestampEnricher>();
+        }
+    }
+}
